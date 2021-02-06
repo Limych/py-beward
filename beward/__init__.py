8092b496370b69ef541434351766f740d26de466
Andrey Khrolenok <andrey@khrolenok.ru>
2021-02-06 16:31:50 +0300

    Fix version number

--- beward/__init__.py
+++ beward/__init__.py
@@ -17,7 +17,7 @@ from beward.doorbell import BewardDoorbell
 __author__ = 'Andrey "Limych" Khrolenok <andrey@khrolenok.ru>'
 # Please add the suffix "+" to the version after release, to make it
 # possible infer whether in development code from the version string
-__version__ = "1.1.0-beta"
+__version__ = "1.1.0b2"
 __website__ = "https://github.com/Limych/py-beward"
 __license__ = "Creative Commons BY-NC-SA License"
 
