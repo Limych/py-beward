76e9c90bbdbe051e4b4a5a89a427cdd3aad94ae2
Andrey Khrolenok <andrey@khrolenok.ru>
2021-03-10 01:34:54 +0300

    Skeleton initial commit.

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,57 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: 'bug'
+assignees: ''
+
+---
+## Checklist
+- [ ] I updated the component to the latest version available
+
+
+
+<!-- Before you open a new issue, search through the existing issues to see if others have had the same problem.
+
+Issues not containing the minimum requirements will be closed:
+
+- Issues without a description (using the header is not good enough) will be closed.
+- Issues without debug logging will be closed.
+- Issues without configuration will be closed
+
+-->
+
+## Version of the component
+<!--
+If you are not using the newest version, download and try that before opening an issue.
+If you are unsure about the version check the __init__.py file.
+-->
+Version:
+
+## Describe the bug
+
+_A clear and concise description of what the bug is._
+
+## To Reproduce
+This is the configuration I used:
+```yaml
+
+Add your configs here.
+
+```
+
+## Debug log
+<!-- To enable debug logs check this https://www.home-assistant.io/components/logger/ -->
+```text
+
+Add your logs here.
+
+```
+
+## Expected behavior
+
+_A clear and concise description of what you expected to happen._
+
+## Additional context
+
+_Add any other context about the problem here._
---
+++ setup.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+"""Library module setup."""
+
+import re
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    """PyTest controller."""
+
+    # Code from here:
+    # https://docs.pytest.org/en/latest/goodpractices.html#manual-integration
+
+    # pylint: disable=attribute-defined-outside-init
+    def finalize_options(self):
+        """Finalize test command options."""
+        TestCommand.finalize_options(self)
+        # we don't run integration tests which need an actual Beward device
+        self.test_args = ['-m', 'not integration']
+        self.test_suite = True
+
+    # pylint: disable=import-outside-toplevel,import-error
+    def run_tests(self):
+        """Run tests."""
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        import shlex
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+def load_requirements(fpath: str) -> list:
+    """Load requirements from file."""
+    data = list(open(fpath))
+    imp = re.compile(r'^(-r|--requirement)\s+(\S+)')
+    reqs = []
+    for i in data:
+        # pylint: disable=invalid-name
+        m = imp.match(i)
+        if m:
+            reqs.extend(load_requirements(m.group(2)))
+        else:
+            reqs.append(i)
+
+    return reqs
+
+
+src = open('beward/__init__.py', encoding='utf-8').read()   # fixme
+metadata = dict(re.findall(r'__([a-z]+)__ = "([^"]+)"', src))
+metadata.update(dict(re.findall(r"__([a-z]+)__ = '([^']+)'", src)))
+docstrings = re.findall(r'"""(.*?)"""', src, re.MULTILINE | re.DOTALL)
+
+NAME = 'beward'  # fixme
+
+PACKAGES = (
+    'beward',  # fixme
+)
+
+AUTHOR_EMAIL = metadata['author']
+VERSION = metadata['version']
+WEBSITE = metadata['website']
+LICENSE = metadata['license']
+DESCRIPTION = docstrings[0]
+
+CLASSIFIERS = [  # fixme
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: Other/Proprietary License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: Home Automation',
+    'Topic :: Security',
+    'Topic :: Multimedia :: Video :: Capture',
+]
+
+with open('README.md', encoding='utf-8') as file:
+    LONG_DESCRIPTION = file.read()
+    LONG_DESCRIPTION_TYPE = 'text/markdown'
+
+# Extract name and e-mail ("Firstname Lastname <mail@example.org>")
+AUTHOR, EMAIL = re.match(r'(.*) <(.*)>', AUTHOR_EMAIL).groups()
+
+REQUIREMENTS = load_requirements('requirements.txt')
+TEST_REQUIREMENTS = load_requirements('requirements-test.txt')
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    license=LICENSE,
+    url=WEBSITE,
+    packages=PACKAGES,
+    install_requires=REQUIREMENTS,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_TYPE,
+    classifiers=CLASSIFIERS,
+    cmdclass={'pytest': PyTest},
+    test_suite="tests",
+    tests_require=TEST_REQUIREMENTS,
+)
