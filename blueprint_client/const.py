7dd3788e972c6fdf79d3d6a549a1d1e4131820ec
Andrey Khrolenok <andrey@khrolenok.ru>
2021-03-23 05:17:14 +0300

    Bump version

--- blueprint_client/const.py
+++ blueprint_client/const.py
@@ -1,7 +1,7 @@
 """Constants."""
 
 # Base library constants
-VERSION = "1.0.1.dev0"
+VERSION = "1.0.1"
 AUTHOR = "Andrey Khrolenok <andrey@khrolenok.ru>"
 LICENSE = "MIT License"
 WEBSITE = "https://github.com/Limych/py-blueprint"
