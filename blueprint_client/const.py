43b3f5e52bedb3ab6b1494cf9ee68204a921fb6f
Andrey Khrolenok <andrey@khrolenok.ru>
2024-10-27 19:59:34 +0300

    Update setup.py and pyproject.toml

--- blueprint_client/const.py
+++ blueprint_client/const.py
@@ -1,18 +1,19 @@
 """Constants."""
 
+from importlib import metadata
+
 # Base library constants
-NAME = "Blueprint Client"
 VERSION = "1.0.2.dev0"
-AUTHOR = "Andrey Khrolenok <andrey@khrolenok.ru>"
-LICENSE = "MIT License"
-WEBSITE = "https://github.com/Limych/py-blueprint"
-ISSUE_URL = "https://github.com/Limych/py-blueprint/issues"
+
+mdata = metadata.metadata(__package__)
+
+URLS = dict(x.split(", ") for x in mdata.get_all("Project-URL"))
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
-{NAME}
-Version: {VERSION}
+{mdata["Summary"]}
+Version: {mdata["Version"]}
 If you have ANY issues with this you need to open an issue here:
-{ISSUE_URL}
+{URLS["Issues"]}
 -------------------------------------------------------------------
 """
--- setup.py
+++ setup.py
@@ -2,37 +2,9 @@
 """Library module setup."""
 
 import re
-import sys
 from pathlib import Path
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand  # noqa: N812
-
-
-class PyTest(TestCommand):
-    """PyTest controller."""
-
-    # Code from here:
-    # https://docs.pytest.org/en/latest/goodpractices.html#manual-integration
-
-    # pylint: disable=attribute-defined-outside-init
-    def finalize_options(self) -> None:
-        """Finalize test command options."""
-        TestCommand.finalize_options(self)
-        # we don't run integration tests which need an actual blueprint_client device
-        self.test_args = ["-m", "not integration"]
-        self.test_suite = True
-
-    # pylint: disable=import-outside-toplevel,import-error
-    def run_tests(self) -> None:
-        """Run tests."""
-        # import here, cause outside the eggs aren't loaded
-        import shlex
-
-        import pytest
-
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
 
 
 def load_requirements(fpath: str) -> list:
@@ -52,54 +24,21 @@ def load_requirements(fpath: str) -> list:
     return reqs
 
 
-with Path("blueprint_client/const.py").open(encoding="utf-8") as file:
+PACKAGES = [x for x in find_packages() if x not in ["scripts", "tests"]]
+
+with (Path(PACKAGES[0]) / "const.py").open(encoding="utf-8") as file:
     src = file.read()
 metadata = dict(re.findall(r'([a-z]+) = "([^"]+)"', src, re.IGNORECASE))
 metadata.update(dict(re.findall(r"([a-z]+) = '([^']+)'", src, re.IGNORECASE)))
 docstrings = re.findall(r'"""(.*?)"""', src, re.MULTILINE | re.DOTALL)
 
-NAME = "blueprint_client"
-
-PACKAGES = [x for x in find_packages() if x not in ["scripts", "tests"]]
-
 VERSION = metadata["VERSION"]
-AUTHOR_EMAIL = metadata.get("AUTHOR", "Unknown <no@email.com>")
-WEBSITE = metadata.get("WEBSITE", "")
-LICENSE = metadata.get("LICENSE", "")
-DESCRIPTION = docstrings[0]
-
-CLASSIFIERS = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-]
-
-with Path("README.md").open(encoding="utf-8") as file:
-    LONG_DESCRIPTION = file.read()
-    LONG_DESCRIPTION_TYPE = "text/markdown"
-
-# Extract name and e-mail ("Firstname Lastname <mail@example.org>")
-AUTHOR, EMAIL = re.match(r"(.*) <(.*)>", AUTHOR_EMAIL).groups()
 
 REQUIREMENTS = load_requirements("requirements.txt")
 TEST_REQUIREMENTS = load_requirements("requirements-test.txt")
 
 setup(
-    name=NAME,
     version=VERSION,
-    description=DESCRIPTION,
-    author=AUTHOR,
-    author_email=EMAIL,
-    license=LICENSE,
-    url=WEBSITE,
     packages=PACKAGES,
     install_requires=REQUIREMENTS,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type=LONG_DESCRIPTION_TYPE,
-    classifiers=CLASSIFIERS,
-    cmdclass={"pytest": PyTest},
-    test_suite="tests",
-    tests_require=TEST_REQUIREMENTS,
 )
